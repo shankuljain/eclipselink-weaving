65a7924ebf25ccfc32111588985d38aafe8935e1
shanks <shankuljain@ymail.com>
2024-12-05 02:28:03 +0530

    First commit

---
+++ src/main/java/com/shanks/weaving/WeavingApplication.java
@@ -0,0 +1,13 @@
+package com.shanks.weaving;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeavingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeavingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shanks/weaving/WeavingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shanks.weaving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeavingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
